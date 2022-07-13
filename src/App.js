bc757fbe14791554117f97857760b499a07a91a4
Jonathan Lopez <jlopez@artedigital-mx.com>
2022-07-13 13:10:15 -0500

    Refactor to scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Provider from './provider';
+import { Header } from './stories/Header/Header';
 
 function App() {
   return (
     <Provider>
       <div className="App">
+        <Header></Header>
       </div>
     </Provider>
   );
